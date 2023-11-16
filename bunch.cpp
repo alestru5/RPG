391691f987b38df33c12af26afa4478cbd2c41cf
alestru5 <sstrugovv@inbox.ru>
2023-11-16 06:26:39 -0500

    re! bunch

--- bunch.cpp
+++ bunch.cpp
@@ -3,21 +3,16 @@
 #include "dungeon.h"
 
 Bunch::Bunch(type_bunch T): Item(type_item::bunch){
-    try{
-        bunch_type = T;
-        count = SetCount::createCount(T);
-    } catch(...){
-        throw;
-    }
+    bunch_type = T;
+    count = SetCount::createCount(T);
 }
 
 Bunch& Bunch::operator= (const Bunch &I) noexcept{
     if (this != &I){
         bunch_type = I.bunch_type;
         count = I.count;
-
-        Item::operator=(I);
     }
+    return *this;
 }
 
 void Bunch::use(Dungeon &dungeon){
