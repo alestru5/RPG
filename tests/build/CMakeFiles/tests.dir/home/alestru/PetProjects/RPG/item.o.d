f165680800765072b3a431a85aaa71dab62f6041
alestru5 <sstrugovv@inbox.ru>
2023-12-04 17:11:41 -0500

    dir commit

--- tests/build/CMakeFiles/tests.dir/home/alestru/PetProjects/RPG/item.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/tests.dir/home/alestru/PetProjects/RPG/item.o: \
- /home/alestru/PetProjects/RPG/item.cpp /usr/include/stdc-predef.h \
- /home/alestru/PetProjects/RPG/item.h \
- /home/alestru/PetProjects/RPG/enums.h
